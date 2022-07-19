02a29cd16045843ff3d1fa3ca475d9e0fd7b1633
Viktoriia-Piskova <viktoriapiskova@gmail.com>
2022-07-19 15:11:28 +0300

    Reversed digits script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//script fo calculating chocolate
+//script for calculating chocolate
 
 document.querySelector("#chocolateDataInputs").addEventListener("change", countChocolates);
 
@@ -12,11 +12,39 @@ function countChocolates() {
         let chocolateResult = Math.floor(userMoney / userPrice);
         document.querySelector("#chocolateResult").innerHTML = `<p>You can buy ${chocolateResult} chocolates</p>`;
         let changeResult = userMoney % userPrice;
-        document.querySelector("#changeResult").innerHTML = `<p>You will get ${changeResult} dollars.</p>`;
+        document.querySelector("#changeResult").innerHTML = `<p>Your change is  ${changeResult} dollars.</p>`;
     }
     else {
         document.querySelector("#chocolateResult").innerHTML = `<p>Please, enter valid data and press Enter. All numbers must be bigger than 0. </p>`;
         document.querySelector("#changeResult").innerHTML = `<p>Unable to count :( </p>`;
     }
 
+}
+
+
+//script for backwards digit
+
+
+document.querySelector("#userDigit").addEventListener("change", backwardDigit);
+
+function backwardDigit(){
+    let userDigit = parseFloat(document.querySelector("#userDigit").value);
+    
+    if(userDigit != undefined){
+
+        let lastDigit = "";
+        let digitResult = `Reversed number is ${lastDigit}`;
+
+        while (userDigit > 0){
+            
+            lastDigit = userDigit % 10;
+            userDigit = Math.floor(userDigit / 10);
+            digitResult += lastDigit;
+            document.querySelector("#digitResult").innerHTML = `Reversed number is ${digitResult}`;
+
+        }
+
+        //document.querySelector("#digitResult").innerHTML = `Reversed number is ${digitResult}`;
+
+    }
 }
